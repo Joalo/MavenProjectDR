c29502b179cedefe16fddef192d3c922e17204ee
Joshua Mwenya <Joshua.m.mwenya@gmail.com>
2020-11-17 04:16:17 +0000

    Addition of Methods to contol Arraylists

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -6,42 +6,84 @@ public class Driver {
 
 
 
-    public void StudentsinModule(Student STU , Module mods){
+    public void StudentsinModule(Student STU , Module mods)
+    {
 
-if (STU.getMODULES().contains(mods))
-{
-    return;
-}
-        else{
-
-            ArrayList<Student> holder = mods.returnStudent();
-            ArrayList<Module> stuinmods = STU.getMODULES();
-            holder.add(STU);
-            stuinmods.add(mods);
-            mods.setSTUDENTLIST(holder);
-            STU.setMODULES(stuinmods);
-
-}
-
-    }
-
-    public void CoursesinModule(){
-
+        if (STU.returnModule().contains(mods))
+        {
+            return;
+        }
+        else
+            {
+                    ArrayList<Student> holder = mods.returnStudent();
+                    ArrayList<Module> modsforSTU = STU.returnModule();
+                    holder.add(STU);
+                    modsforSTU.add(mods);
+                    mods.setSTUDENTLIST(holder);
+                    STU.setMODULES(modsforSTU);
+            }
 
     }
 
-    public void StudentsinCourse(){
+    public void CoursesinModule()
+    {
 
 
     }
 
-    public void ModulesinCourse(){
+    public void StudentsinCourse(Student STU, Course cour)
+    {
+       ArrayList<Course> courinmods = STU.returnCourse();
+       if( courinmods.contains(cour) == true)
+       {
+           return;
+       }
+       else
+       {
+           courinmods.add(cour);
+           STU.setCOURSES(courinmods);
+       }
+    }
+
+    public void ModulesinCourse(Module mods, Course cour)
+    {
+        if (mods.returnCourse().contains(cour) == true)
+        {
+            return;
+        }
+        else
+            {
+            ArrayList<Course> holder = mods.returnCourse();
+            holder.add(cour);
+            mods.setCOURSELIST(holder);
+
+            ArrayList<Module> modsincour = cour.returnModule();
+            modsincour.add(mods);
+            cour.setMODULELIST(modsincour);
+
+            ArrayList<Student> stuinmods = mods.returnStudent();
+            ArrayList<Student> stuincour = cour.returnStudent();
+
+            for (Student studentinmod : stuinmods)
+            {
+                stuincour.add(studentinmod);
+            }
+            
+            cour.setSTUDENTLIST(stuincour);
+
+            for (Student studentincour : stuincour)
+            {
+                StudentsinCourse(studentincour,cour);
+            }
+
+        }
 
 
     }
 
 
-    public static void main(String args[]) {
+    public static void main(String[] args)
+    {
 
 
     }
