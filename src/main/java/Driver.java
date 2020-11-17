f99ea951eae7c3dcd199c7d7a0c154fe1a2e4dd8
Joshua Mwenya <Joshua.m.mwenya@gmail.com>
2020-11-17 02:06:28 +0000

    Intial commit

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -5,6 +5,42 @@ import java.util.ArrayList;
 public class Driver {
 
 
+
+    public void StudentsinModule(Student STU , Module mods){
+
+if (STU.getMODULES().contains(mods))
+{
+    return;
+}
+        else{
+
+            ArrayList<Student> holder = mods.returnStudent();
+            ArrayList<Module> stuinmods = STU.getMODULES();
+            holder.add(STU);
+            stuinmods.add(mods);
+            mods.setSTUDENTLIST(holder);
+            STU.setMODULES(stuinmods);
+
+}
+
+    }
+
+    public void CoursesinModule(){
+
+
+    }
+
+    public void StudentsinCourse(){
+
+
+    }
+
+    public void ModulesinCourse(){
+
+
+    }
+
+
     public static void main(String args[]) {
 
 
